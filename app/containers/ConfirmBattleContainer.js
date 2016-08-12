8f4757335accc6fa9eb598741fbd76d6348e1c91
Ji You <kroyagis@gmail.com>
2016-08-12 14:06:54 -0400

    -added styling to ConfirmBattle stage

--- app/containers/ConfirmBattleContainer.js
+++ app/containers/ConfirmBattleContainer.js
@@ -10,7 +10,7 @@ var ConfirmBattleContainer = React.createClass({
   getInitialState: function(){
     return{
       isLoading: true,
-      playerInfo: []
+      playersInfo: [],
     }
   },
   componentDidMount: function(){
@@ -23,9 +23,18 @@ var ConfirmBattleContainer = React.createClass({
         })
       }.bind(this))
   },
+  handleInitiateBattle: function(){
+    this.context.router.push({
+      pathname: '/results',
+      state:{
+        playersInfo: this.state.playersInfo
+      }
+    })
+  },
   render: function(){
     return(
       <ConfirmBattle isLoading={this.state.isLoading}
+      onInitiateBattle={this.handleInitiateBattle}
       playersInfo={this.state.playersInfo}/>
     );
   }
--- app/utils/githubHelpers.js
+++ app/utils/githubHelpers.js
@@ -5,20 +5,20 @@ var sec = "YOUR_SECRET_ID";
 var param = "?client_id=" + id + "&client_secret=" + sec;
 
 function getUserInfo (username) {
-  return axios.get('https://api.github.com/users/' + username + param)
+  return axios.get('https://api.github.com/users/' + username + param);
 }
 
 var helpers = {
-  getPlayersInfo: function(players){
-    return axios.all(players.map(function(username) {
+  getPlayersInfo: function (players) {
+    return axios.all(players.map(function (username) {
       return getUserInfo(username)
-    })).then(function(info){
-      return info.map(function(user){
-        return user.data;
+    }))
+    .then(function (info) {
+      return info.map(function (user) {
+        return user.data
       })
-    }).catch(function(err){
-      console.warn('Error in getPlayersInfo', err)
     })
+    .catch(function (err) {console.warn('Error in getPlayersInfo: ', err)})
   }
 };
 
